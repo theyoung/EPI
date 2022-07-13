dcf8740175b1e5abdec579671b8cc15cbbe45f9f
stevenna <theyoung2002@naver.com>
2022-07-13 23:16:54 +0900

    Chapter 08: Binary Trees

--- epi_judge_java/epi/TreeFromPreorderWithNull.java
+++ epi_judge_java/epi/TreeFromPreorderWithNull.java
@@ -4,13 +4,35 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 public class TreeFromPreorderWithNull {
   public static BinaryTreeNode<Integer>
   reconstructPreorder(List<Integer> preorder) {
     // TODO - you fill in here.
-    return null;
+    Deque<Integer> queue = new LinkedList<>(preorder);
+
+
+    return helper(queue);
   }
+
+  public static BinaryTreeNode<Integer> helper(Deque<Integer> queue){
+    if(queue.size() == 0) return null;
+
+    var cur = queue.pollFirst();
+
+    if(cur == null) return null;
+
+    BinaryTreeNode<Integer> node = new BinaryTreeNode<>(cur);
+
+    node.left = helper(queue);
+    node.right = helper(queue);
+
+    return node;
+  }
+
+
   @EpiTest(testDataFile = "tree_from_preorder_with_null.tsv")
   public static BinaryTreeNode<Integer>
   reconstructPreorderWrapper(TimedExecutor executor, List<String> strings)
